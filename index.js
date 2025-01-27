5b599e50f0eb8a650b45b74d702cad025b59d66c
iamshivamgaur <shivam24august@gmail.com>
2025-01-27 08:25:56 +0530

    Adding file with morgan and wiston

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-import express from "express";
+import express, { response } from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
+import { configDotenv } from "dotenv";
 
 const app = express();
-let port = 3000;
+
+configDotenv();
+const port = process.env.PORT || 3000;
 
 //middleware
 app.use(express.json());
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let teaData = [];
 let nextId = 1;
 
 // Add a new tea
 app.post("/teas", (req, res) => {
+  logger.warn("A post request is made to add new tea");
   const { name, price } = req.body;
   const newTea = { id: nextId++, name, price };
   teaData.push(newTea);
@@ -19,11 +43,13 @@ app.post("/teas", (req, res) => {
 
 // get all tea
 app.get("/teas", (req, res) => {
+  // console.log("GET ALL");
   res.status(200).send(teaData);
 });
 
 // get one tea with id
 app.get("/teas/:id", (req, res) => {
+  // console.log("GET SINGLE");
   const tea = teaData.find((t) => t.id === parseInt(req.params.id));
   if (!tea) {
     return res.status(404).send("Tea not found");
@@ -33,6 +59,7 @@ app.get("/teas/:id", (req, res) => {
 
 //update tea
 app.put("/teas/:id", (req, res) => {
+  // console.log("PUT");
   const tea = teaData.find((t) => t.id === parseInt(req.params.id));
   if (!tea) {
     return res.status(404).send("Tea not found");
@@ -43,9 +70,9 @@ app.put("/teas/:id", (req, res) => {
   res.status(200).send(tea);
 });
 
-
 //delete tea
 app.delete("/teas/:id", (req, res) => {
+  // console.log("DELETE");
   const index = teaData.findIndex((t) => t.id === parseInt(req.params.id));
   if (index === -1) {
     return res.status(404).send("Tea not found");
